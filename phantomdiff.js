1205db068d38ef0850d7b95ee05dfdf7a76a4633
Sébastien renault <sre@zenexity.com>
2013-02-14 17:43:10 +0100

    split phantomdiff from tests

--- phantomdiff.js
+++ phantomdiff.js
@@ -2,113 +2,116 @@
  * phantomdiff.js
  */
 
-exports.create = function create() {
-    "use strict";
-    return new PhantomDiff();
+var fs = require("fs");
+
+exports.init = function create(casper) {
+    return new PhantomDiff(casper);
 };
 
 exports.PhantomDiff = PhantomDiff;
 
-phantomDiff.test.exporter = require('./html').create();
-phantomDiff.test.assertImage = function(current, baseline, message) {
-    var self = this;
-    startDiffServer(baseline, current, function(result) {
-        self.assertTrue(result.isEquals, message, {
-            type: "assertImage",
-            standard: "Image equals the expected image",
-            values: {
-                subject:  current,
-                expected: baseline,
-                diff: result.diff
-            }
+var PhantomDiff = function(casper) {
+
+    var startDiffServer = function(baseline, current, callback) {
+        var browser = require('casper').create({
+            viewportSize: { width: 1027, height: 800 }
         });
-    });
-};
-
-var startDiffServer = function(baseline, current, callback) {
-    var browser = require('casper').create({
-        viewportSize: { width: 1027, height: 800 }
-        //            verbose: true,
-        //            logLevel: 'info'
-    });
-    var server = require('webserver').create();
-    var fs = require("fs");
-    var html = fs.read('images.html');
-    var service = server.listen(1337, function(request, response) {
-        response.statusCode = 200;
-        response.write(html);
-        response.close();
-    });
-
-    browser.start('http://localhost:1337', function() {
-        browser.page.injectJs('/Users/sre/data/Projects/me/phantomdiff/imagediff.min.js');
-        this.fill('form#images-diff', {
-            'baseline': baseline,
-            'current': current
+        var server = require('webserver').create();
+        var html = fs.read('images.html');
+        var service = server.listen(1337, function(request, response) {
+            response.statusCode = 200;
+            response.write(html);
+            response.close();
         });
 
-        this.evaluate(function() {
-            var baselineReader = new FileReader(),
-                currentReader = new FileReader(),
-                baselineURL = document.getElementById('baseline').files[0],
-                currentURL = document.getElementById('current').files[0],
-                toDataURLFixed = false;
-
-            window.imageDiff = {
-                hasResult: false,
-                result: {
-                    isEquals: false,
-                    diff: null
-                }
-            };
-
-            var getImageData = function(callback) {
-                return function(b) {
-                    var img = new Image();
-                    img.onload = function() {
-                        callback && callback(img);
-                    };
-                    img.src = b.target.result;
-                };
-            };
-
-            baselineReader.onload = getImageData(function(baselineImg) {
-                currentReader.onload = getImageData(function(currentImg) {
-                    window.imageDiff.result.isEquals = imagediff.equal(baselineImg, currentImg, 100);
-                    if(!window.imageDiff.result.isEquals && toDataURLFixed) {
-                        var rawDiff = imagediff.diff(baselineImg, currentImg);
-                        var canvasDiff = imagediff.createCanvas(rawDiff.width, rawDiff.height);
-                        var context = canvasDiff.getContext('2d');
-                        context.putImageData(rawDiff, 0, 0);
-                        window.imageDiff.result.diff = canvasDiff.toDataURL();
-                    }
-                    window.imageDiff.hasResult = true;
-                });
-                currentReader.readAsDataURL(currentURL);
+        browser.start('http://localhost:1337', function() {
+            browser.page.injectJs('/Users/sre/data/Projects/me/phantomdiff/imagediff.min.js');
+            this.fill('form#images-diff', {
+                'baseline': baseline,
+                'current': current
             });
-            baselineReader.readAsDataURL(baselineURL);
-        });
-    });
 
-    var hasResult = false;
-    browser.waitFor(function check() {
-        return browser.evaluate(function() {
-            return window.imageDiff.hasResult;
-        });
-    }, function then() {
-        var result = browser.evaluate(function() {
-            return window.imageDiff.result;
-        });
-        hasResult = true;
-        callback && callback(result);
-    }, function onTimeout() {
-        console.log('The processing to diff images timeout !');
-    }, 10000);
+            this.evaluate(function() {
+                var baselineReader = new FileReader(),
+                    currentReader = new FileReader(),
+                    baselineURL = document.getElementById('baseline').files[0],
+                    currentURL = document.getElementById('current').files[0],
+                    toDataURLFixed = false;
 
-    browser.run(function() {
-    });
+                window.imageDiff = {
+                    hasResult: false,
+                    result: {
+                        isEquals: false,
+                        diff: null
+                    }
+                };
 
-    phantomDiff.waitFor(function() {
-        return hasResult;
-    }, null, null, 3600000);
-};
+                var getImageData = function(callback) {
+                    return function(b) {
+                        var img = new Image();
+                        img.onload = function() {
+                            callback && callback(img);
+                        };
+                        img.src = b.target.result;
+                    };
+                };
+
+                baselineReader.onload = getImageData(function(baselineImg) {
+                    currentReader.onload = getImageData(function(currentImg) {
+                        window.imageDiff.result.isEquals = imagediff.equal(baselineImg, currentImg, 100);
+                        if(!window.imageDiff.result.isEquals && toDataURLFixed) {
+                            var rawDiff = imagediff.diff(baselineImg, currentImg);
+                            var canvasDiff = imagediff.createCanvas(rawDiff.width, rawDiff.height);
+                            var context = canvasDiff.getContext('2d');
+                            context.putImageData(rawDiff, 0, 0);
+                            window.imageDiff.result.diff = canvasDiff.toDataURL();
+                        }
+                        window.imageDiff.hasResult = true;
+                    });
+                    currentReader.readAsDataURL(currentURL);
+                });
+                baselineReader.readAsDataURL(baselineURL);
+            });
+        });
+
+        var hasResult = false;
+        browser.waitFor(function check() {
+            return browser.evaluate(function() {
+                return window.imageDiff.hasResult;
+            });
+        }, function then() {
+            var result = browser.evaluate(function() {
+                return window.imageDiff.result;
+            });
+            hasResult = true;
+            callback && callback(result);
+        }, function onTimeout() {
+            console.log('The processing to diff images timeout !');
+        }, 10000);
+
+        browser.run(function() {
+        });
+
+        casper.waitFor(function() {
+            return hasResult;
+        }, null, null, 3600000);
+    };
+
+    casper.test.exporter = require('./html').create();
+
+    casper.test.assertImage = function(current, baseline, message) {
+        var self = this;
+        startDiffServer(baseline, current, function(result) {
+            self.assertTrue(result.isEquals, message, {
+                type: "assertImage",
+                standard: "Image equals the expected image",
+                values: {
+                    subject:  current,
+                    expected: baseline,
+                    diff: result.diff
+                }
+            });
+        });
+    };
+}
+
--- testrunner.js
+++ testrunner.js
@@ -2,7 +2,6 @@
  * testrunner.js
  */
 
-
 (function() {
     var Casper = require('casper').Casper,
         utils = require("utils"),
@@ -21,113 +20,20 @@
         };
     };
 
-    var PhantomDiff = function() {
-        PhantomDiff.super_.apply(this, arguments);
+    var TestRunner = function() {
+        TestRunner.super_.apply(this, arguments);
     };
-    utils.inherits(PhantomDiff, Casper);
-    var phantomDiff = new PhantomDiff({
+
+    utils.inherits(TestRunner, Casper);
+    var testRunner = new TestRunner({
         viewportSize: { width: 1027, height: 800 },
         verbose: true,
         logLevel: 'debug'
     });
 
-    phantomDiff.test.exporter = require('./html').create();
-    phantomDiff.test.assertImage = function(baseline, current, message) {
-        var self = this;
-        startDiffServer(baseline, current, function(result) {
-            self.assertTrue(result.isEquals, message, {
-                type: "assertImage",
-                standard: "Image equals the expected image",
-                values: {
-                    subject:  current,
-                    expected: baseline,
-                    diff: result.diff
-                }
-            });
-        });
-    };
+    require('./phantomdiff').init(testRunner);
 
-    var startDiffServer = function(baseline, current, callback) {
-        var browser = require('casper').create({
-            viewportSize: { width: 1027, height: 800 }
-//            verbose: true,
-//            logLevel: 'info'
-        });
-        var server = require('webserver').create();
-        var fs = require("fs");
-        var html = fs.read('images.html');
-        var service = server.listen(1337, function(request, response) {
-            response.statusCode = 200;
-            response.write(html);
-            response.close();
-        });
-
-        browser.start('http://localhost:1337', function() {
-            browser.page.injectJs('/Users/sre/data/Projects/me/phantomdiff/imagediff.min.js');
-            this.fill('form#images-diff', {
-                'baseline': baseline,
-                'current': current
-            });
-
-            this.evaluate(function() {
-                var baselineReader = new FileReader(),
-                    currentReader = new FileReader(),
-                    baselineURL = document.getElementById('baseline').files[0],
-                    currentURL = document.getElementById('current').files[0];
-
-                window.imageDiff = {
-                    hasResult: false,
-                    result: {
-                        isEquals: false,
-                        diff: null
-                    }
-                };
-
-                var getImageData = function(callback) {
-                    return function(b) {
-                        var img = new Image();
-                        img.onload = function() {
-                            callback && callback(img);
-                        };
-                        img.src = b.target.result;
-                    };
-                };
-
-                baselineReader.onload = getImageData(function(baselineImg) {
-                    currentReader.onload = getImageData(function(currentImg) {
-                        window.imageDiff.result.isEquals = imagediff.equal(baselineImg, currentImg, 100);
-                        window.imageDiff.hasResult = true;
-                    });
-                    currentReader.readAsDataURL(currentURL);
-                });
-                baselineReader.readAsDataURL(baselineURL);
-            });
-        });
-
-        var hasResult = false;
-        browser.waitFor(function check() {
-            return browser.evaluate(function() {
-                return window.imageDiff.hasResult;
-            });
-        }, function then() {
-            var result = browser.evaluate(function() {
-                return window.imageDiff.result;
-            });
-            hasResult = true;
-            callback && callback(result);
-        }, function onTimeout() {
-            console.log('The processing to diff images timeout !');
-        }, 10000);
-
-        browser.run(function() {
-        });
-
-        phantomDiff.waitFor(function() {
-            return hasResult;
-        }, null, null, 3600000);
-    };
-
-    phantomDiff.start('http://twelvesouth.wroom.dev', function() {
+    testRunner.start('http://twelvesouth.wroom.dev', function() {
         var baseline = Config.baselineFilename(this.getCurrentUrl()),
             current = Config.currentFilename(this.getCurrentUrl());
 
@@ -140,14 +46,14 @@
         this.test.assertImage(baseline, current, 'The image ' + baseline + ' should be equal to ' + current);
     });
 
-    phantomDiff.then(function() {
+    testRunner.then(function() {
         this.test.assertTrue(false, "A should be false");
         this.test.assertTrue(false, "B should be false");
     });
 
-    phantomDiff.run(function() {
+    testRunner.run(function() {
         this.test.done();
         this.test.renderResults(true, 0, './report/report.html');
-        phantomDiff.exit();
+        testRunner.exit();
     });
 })();
