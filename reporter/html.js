5de5b0816873077aef4482e03cec8f38b484c22c
Sébastien renault <sre@zenexity.com>
2013-02-15 09:31:14 +0100

    Having organized data in order to export them

--- reporter/html.js
+++ reporter/html.js
@@ -3,60 +3,103 @@ var utils = require('utils'),
     _ = require('./libs/underscore');
 
 
-/**
- * Creates a HTML instance
- *
- * @return HtmlExporter
- */
 exports.create = function create() {
     "use strict";
     return new HTMLExporter();
 };
 
-/**
- * HTML exporter for test results.
- *
- */
 function HTMLExporter() {
     "use strict";
 
-    var passedTmpl = fs.read('./report/tmpl/passed.html'),
-        failedTmpl = fs.read('./report/tmpl/failed.html');
+    //Final result
+    this.testsResult = {
+        passed: 0,
+        failed: 0,
+        passesTime: 0,
+        failuresTime: 0,
+        testSuites: [
+        ]
+    };
+
+    //Templates
+    this.passedTmpl = fs.read('./report/tmpl/passed.html');
+    this.failedTmpl = fs.read('./report/tmpl/failed.html');
 }
 
 exports.HTMLExporter = HTMLExporter;
 
-HTMLExporter.prototype.copyImagesToReport = function copyImagesToReport(baseline, current) {
-    var baselinePath = baseline.split('/'),
-        currentPath = current.split('/'),
-        baselineFilename = baselinePath[baselinePath.length - 1],
-        currentFilename = currentPath[currentPath.length - 1],
-        baselineReport = 'report/images/baseline/' + baselineFilename,
-        currentReport = 'report/images/current/' + currentFilename;
-
-    try { fs.remove(baselineReport); } catch(e) {}
-    try { fs.remove(currentReport); } catch(e) {}
-
-    fs.copy(baseline, baselineReport);
-    fs.copy(current, currentReport);
-};
-
-HTMLExporter.prototype.addSuccess = function addSuccess(classname, name, duration, _success_) {
-    "use strict";
-};
-
-HTMLExporter.prototype.addFailure = function addFailure(classname, name, message, type, duration, _failure_) {
-    "use strict";
-};
-
-HTMLExporter.prototype.setSuiteDuration = function setSuiteDuration(duration) {
-};
-
 HTMLExporter.prototype.setTestResults = function setTestResults(testResults) {
     //console.log(JSON.stringify(testResults));
+
+    var summup = function(a, b) {
+        return a + b;
+    };
+
+    var filename = function(path) {
+        var splitPath = path.split('/');
+        return splitPath[splitPath.length - 1];
+    };
+
+    var copyImagesToReport = function copyImagesToReport(images) {
+        var baselineFilename = filename(images.baseline),
+            currentFilename = filename(images.current),
+            baselineReport = 'report/images/baseline/' + baselineFilename,
+            currentReport = 'report/images/current/' + currentFilename;
+
+        try { fs.remove(baselineReport); } catch(e) {}
+        try { fs.remove(currentReport); } catch(e) {}
+
+        fs.copy(images.baseline, baselineReport);
+        fs.copy(images.current, currentReport);
+    };
+
+    var imagesForReport = function(testResults) {
+        var all = testResults.passes.concat(testResults.failures);
+        return all.filter(function(test) {
+            return test.type == 'assertImage';
+        }).map(function(test) {
+            return {
+                current: test.values.subject,
+                baseline: test.values.expected
+            };
+        });
+    };
+
+    imagesForReport(testResults).forEach(function(images) {
+        copyImagesToReport(images);
+    });
+
+    var asTestsSuite = function(testResults) {
+        var all = testResults.passes.concat(testResults.failures);
+
+        var testsWithSuiteName =  _(all).map(function(test) {
+            var name = filename(test.file),
+                extLength = 3;
+            test.suite = name.substring(0, name.length - extLength);
+            return test;
+        });
+
+        return _(testsWithSuiteName).groupBy(function(test) {
+            return test.suite;
+        });
+    };
+
+    this.testResults = {
+        passed: testResults.passed,
+        failed: testResults.failed,
+        passesTime: testResults.passesTime.reduce(summup, 0),
+        failuresTime: testResults.failuresTime.reduce(summup, 0),
+        totalTime: testResults.passesTime.concat(testResults.failuresTime).reduce(summup, 0),
+        testSuites: asTestsSuite(testResults)
+    };
 };
 
 HTMLExporter.prototype.getXML = function getXML() {
     "use strict";
     return this._html;
 };
+
+// Didn't used anymore
+HTMLExporter.prototype.addSuccess = function addSuccess(classname, name, duration){};
+HTMLExporter.prototype.addFailure = function addFailure(classname, name, message, type, duration){};
+HTMLExporter.prototype.setSuiteDuration = function setSuiteDuration(duration){};
--- tests/usecase1.js
+++ tests/usecase1.js
@@ -4,4 +4,5 @@
 
 casper.test.comment('use case 1.js');
 casper.test.assertTrue(false, "A should be false");
+casper.test.assertTrue(false, "C should be false");
 casper.test.done(1);
